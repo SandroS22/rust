a6bed62b11cd58b711f82e1f694374b69a2c6a4e
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2023-12-29 15:45:18 -0300

    first time rust

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use text_io::read;
+extern crate text_io;
+
+fn main() {
+    println!("");
+
+    let _number: i32 = read!("Please insert the number: {}");
+
+    // fib(number);
+}
+
+// fn fib(number: i32) {
+//     println!("Java {}", number);
+// }
