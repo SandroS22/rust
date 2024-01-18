2532f40ddcec763c6cb3a6f48cd1cea8605a6636
Sandro Santana Ribeiro <sandro.santana@grad.ufsc.br>
2024-01-18 17:51:31 -0300

    first time in rust

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
-use text_io::read;
-extern crate text_io;
-
 fn main() {
-    println!("");
 
-    let _number: i32 = read!("Please insert the number: {}");
+    // 43 is the limit for this num type
+    let number = 43;
 
-    // fib(number);
+
+    println!("Result: {}", fib(number));
 }
 
-// fn fib(number: i32) {
-//     println!("Java {}", number);
-// }
+fn fib(number: i32) -> i32 {
+    let mut previous = 1;
+    let mut i = 2;
+    let mut counter = 0;
+    let mut num = 0;
+
+    if number == 0 {
+        return 0;
+    } else if number == 1{
+        return 1;
+    }
+    while counter < number { 
+        num = i + previous; 
+        previous = i;
+        i = num;
+        counter += 1;
+        println!("{}", i);
+
+    }
+    return num;
+}
